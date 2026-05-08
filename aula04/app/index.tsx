85c17e6deb730150e7620ef60822169a2018b0c8
passos <wendellpassos16@gmail.com>
2026-05-07 22:08:05 -0300

    tarefa do joao siles

--- aula04/app/index.tsx
+++ aula04/app/index.tsx
@@ -9,7 +9,28 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Olá mundo!!</Text>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
